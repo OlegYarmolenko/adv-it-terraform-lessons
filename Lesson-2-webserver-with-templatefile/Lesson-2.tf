999eb047b540c27fedcf0cbc2263cabe206e8246
OlegYarmolenko <o.yrmlnko@gmail.com>
2021-11-12 02:55:06 +0200

    rename

---
+++ Lesson-2-webserver-with-templatefile/Lesson-2.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ssm_parameter" "my_webserver" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_instance" "my_webserver" {
+  ami                    = data.aws_ssm_parameter.my_webserver.value
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Oleh"
+    l_name = "Yarmolenko"
+    names  = ["Vasya", "Kolya", "Petya"]
+  })
+  
+  tags = {
+    Name = "My webserver"
+  }
+
+}
+
+resource "aws_security_group" "my_webserver" {
+  name        = "Webserver SG"
+  description = "Allow TCP/80"
+
+  ingress {
+    description = "Allow 80 from anyone IP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+output "Webserver-Public-IP" {
+  value = aws_instance.my_webserver.public_ip
+}
