f80c0491227909ac4abccbd1a50ae2b6bd40c3bc
OlegYarmolenko <o.yrmlnko@gmail.com>
2021-11-12 03:43:09 +0200

    add lessson12

--- Lesson-12-Lifecycle/Lesson-12.tf
+++ Lesson-12-Lifecycle/Lesson-12.tf
@@ -6,18 +6,27 @@ data "aws_ssm_parameter" "my_webserver" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
+resource "aws_eip" "webserver_ip" {
+  instance = aws_instance.my_webserver.id
+  
+}
+
 resource "aws_instance" "my_webserver" {
   ami                    = data.aws_ssm_parameter.my_webserver.value
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.my_webserver.id]
   user_data = templatefile("user_data.sh.tpl", {
-    f_name = "Oleh"
+    f_name = "Oleh2"
     l_name = "Yarmolenko"
     names  = ["Vasya", "Kolya", "Petya"]
   })
   
   tags = {
     Name = "My webserver"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 
 }
@@ -55,4 +64,8 @@ resource "aws_security_group" "my_webserver" {
 
 output "Webserver-Public-IP" {
   value = aws_instance.my_webserver.public_ip
+}
+
+output "Webserver-Elastic-IP" {
+  value = aws_eip.webserver_ip.public_ip
 }
